6122d240a57f9ca4a99f73346aaddc7aa2278b7c
louis.le@kmail.vn <khoe.le.mall@gmail.com>
2023-04-08 23:02:26 +0700

    #imp : config vite can work with react-spinkit

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-  }
+  },
+  define: {
+    'process.env': {
+      REACT_SPINKIT_NO_STYLES: process.env.REACT_SPINKIT_NO_STYLES || false,
+      URL_HOST_COINGECKO : process.env.URL_HOST_COINGECKO || 'https://api.coingecko.com/api/v3/coins/'
+    },
+  },
+
 })
